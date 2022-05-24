8358a99b7efbdc04dc0309a46b1bfec40e104588
MITSUNARI Shigeo <herumi@nifty.com>
2022-05-24 10:37:00 +0900

    force unroll

--- src/add-intrin.cpp
+++ src/add-intrin.cpp
@@ -20,7 +20,7 @@ typedef uint64_t Unit;
 #pragma GCC optimize ("unroll-loops") // or -funroll-loops
 #endif
 template<size_t N>
-Unit addT(Unit *z, const Unit *x, const Unit *y)
+static inline Unit addT(Unit *z, const Unit *x, const Unit *y)
 {
 	uint8_t c = 0;
 
@@ -37,4 +37,18 @@ Unit addT(Unit *z, const Unit *x, const Unit *y)
 extern "C" Unit add4(Unit *z, const Unit *x, const Unit *y)
 {
 	return addT<4>(z, x, y);
+}
+
+template<size_t N, size_t I = 0>
+Unit Unroll(uint8_t c, Unit *z, const Unit *x, const Unit *y) {
+	if constexpr(I < N) {
+		c = _addcarry_u64(c, x[I], y[I], (unsigned long long *)&z[I]);
+		return Unroll<N, I + 1>(c, z, x, y);
+	}
+	return c;
+}
+
+extern "C" Unit add4_2(Unit *z, const Unit *x, const Unit *y)
+{
+	return Unroll<4>(0, z, x, y);
 }
